9175a0bbf3bad24b375d72e547300fef0d2eb655
kaiqui <kaiqui82@gmail.com>
2024-05-17 06:28:01 -0300

    feat: add import to main

--- app/main.py
+++ app/main.py
@@ -2,8 +2,10 @@ from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 try:
     from app.endpoints.validation import router as jwt_router
+    from app.endpoints.health import router as health
 except ModuleNotFoundError:
     from endpoints.validation import router as jwt_router
+    from endpoints.health import router as health
 
 app = FastAPI()
 
@@ -12,3 +14,4 @@ async def docs_redirect():
     return RedirectResponse(url='/docs')
 
 app.include_router(jwt_router)
+app.include_router(health)
