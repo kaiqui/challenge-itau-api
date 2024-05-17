ff517fa53d2893fe0d4c3f9954975a8c23938909
kaiqui <kaiqui82@gmail.com>
2024-05-17 15:25:01 -0300

    feat: add trace jwt

--- app/models/claim.py
+++ app/models/claim.py
@@ -6,6 +6,33 @@ try:
     from app.utils.environment import Environment
 except ModuleNotFoundError:
     from utils.environment import Environment
+from prometheus_client import Counter
+
+
+jwt_validation_success_counter = Counter(
+    "jwt_validation_success_counter",
+    "Number of successful JWT validations"
+)
+
+jwt_validation_failure_counter = Counter(
+    "jwt_validation_failure_counter",
+    "Total number of failed JWT validations"
+)
+
+jwt_validation_error_counter = Counter(
+    "jwt_validation_error_counter",
+    "Number of JWT validation errors"
+)
+
+jwt_invalid_token_error_counter = Counter(
+    "jwt_invalid_token_error_counter",
+    "Number of JWT invalid token errors"
+)
+
+jwt_unknown_error_counter = Counter(
+    "jwt_unknown_error_counter",
+    "Number of unknown JWT errors"
+)
 
 class JWTClaims(BaseModel):
     Name: str = Field(..., max_length=256, pattern=r"^[^\d]+$")
@@ -31,19 +58,28 @@ class JWTClaims(BaseModel):
             i += 6
         return True
 
-    @classmethod
-    def validate_jwt(cls, token: str) -> bool:
+    @staticmethod
+    def _validate_jwt(token: str) -> bool:
         try:
             env = Environment("JWT_SECRET")
             secret = env.get("JWT_SECRET")
             decoded_token = jwt.decode(token, secret, algorithms=["HS256"])
-            jwt_claims = cls.from_dict(decoded_token)
+            jwt_claims = JWTClaims.from_dict(decoded_token)
             if (
                 len(decoded_token.keys()) == 3 and
-                cls.is_prime(jwt_claims.Seed) and
+                JWTClaims.is_prime(jwt_claims.Seed) and
                 len(jwt_claims.Name) <= 256
             ):
+                jwt_validation_success_counter.inc()
                 return True
-        except (InvalidTokenError, ValidationError):
-            pass
+        except ValidationError:
+            jwt_validation_failure_counter.inc()
+        except InvalidTokenError:
+            jwt_invalid_token_error_counter.inc()
+        except Exception:
+            jwt_unknown_error_counter.inc()
         return False
+
+    @classmethod
+    def validate_jwt(cls, token: str) -> bool:
+        return cls._validate_jwt(token)
