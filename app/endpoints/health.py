2f404cf44c4b78ac4665aa6fa94ee8eaa77680db
kaiqui <kaiqui82@gmail.com>
2024-05-17 06:05:14 -0300

    style: fix lint

--- app/endpoints/health.py
+++ app/endpoints/health.py
@@ -1,7 +1,13 @@
+"""Health Check
+
+    Returns:
+        json: simple health
+"""
 from fastapi import APIRouter
 
 router = APIRouter()
 
 @router.post("/health")
 async def health():
+    """Health Check"""
     return {"ping": "pong"}
